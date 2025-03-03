b352e4f244034e5e0958c2a6a268ba3a448cbea4
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 21:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,47 +4,31 @@ class Person {
 		this.name=name,
 		this.age=age
 	}
-	get : name(){
+	get name(){
 		return this.name;
 	}
-	get : age(){
-		return this.age;
-	}
-
-	set : age(age1){
-		this.age=age1
+	set age(a){
+		this.age=a;
 	}
 }
 
 class Student extends Person {
-	// constructor(name){
-	// 	super(name);
-		
-	// }
+	constructor(name){
+		super(name);
+	}
 	study(){
 		console.log(`${this.name} is studying`)
 	}
 }
 
 class Teacher extends Person {
-	// constructor(name){
-	// 	super(name)
-	// }
+	constructor(name){
+		super(name)
+	}
 	teach(){
 		console.log(`${this.name} is teaching`)
 	}
 }
-// 
-const person = new Person("Jhon, 25");
-console.log(person.name);
-person.age = 30;
-console.log(person.age);
-
-const student = new Student("Alice, 22");
-student.study();
-
-const teacher = new Teacher("Bob, 40");
-teacher.teach();
 
 
 // Do not change the code below this line
